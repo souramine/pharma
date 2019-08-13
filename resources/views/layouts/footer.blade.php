aaf6ab1e2cdef62aeb5ddf06b8b5db01a287a271
ibra741 <souramine13@gmail.com>
2019-08-13 23:32:45 +0100

    add layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+  <!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Que ton aliment soit ta seule médecine !
+    </div>
+    <!-- Default to the left -->
+    <strong>  Copyright &copy; 2019-2020 <a href="#">Universite Tlemcen</a>.</strong> Touts droits réservés.
+    <!-- REQUIRED SCRIPTS -->
+	<!-- jQuery -->
+	<script src="{{ asset('js/jquery/jquery.min.js')}}"></script>
+	<!-- Bootstrap 4 -->
+	<script src="{{ asset('js/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+	<!-- AdminLTE App -->
+	<script src="{{ asset('js/adminlte.min.js')}}"></script>
+
+	@yield('js')
+</footer>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+@include('layouts.head')
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  
+  @include('layouts.navbar')
+  @include('layouts.aside2')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">{{ $titrePage }}</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">{{ $nomPage }}</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+               @yield('content')
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  
+  @include('layouts.aside')
+  @include('layouts.footer')
+</div>
+<!-- ./wrapper -->
+
+</body>
+
+</html>
